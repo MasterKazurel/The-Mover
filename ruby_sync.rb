6ee388997d8270213e5b1d7d159e570ab6bffabc
Karl Brandenberger <karl.brandenberger@gmail.com>
2018-07-03 15:00:41 -0600

    Added loading bar gem

--- ruby_sync.rb
+++ ruby_sync.rb
@@ -1,12 +1,25 @@
 require "rsync"
+require "thor"
+require "paint"
+require "progress_bar"
 
 input_array = ARGV
 
 def sync_list(file_path)
   f = File.open(file_path, "r")
+
+  # wc -l is an sh command for wordcount and -l as lines
+  # This method is fast and memory efficient
+  cmd = "wc -l " + file_path
+  linecount = %x[ #{cmd} ].strip.split(' ')[0].to_i
+
+  # Create a progress bar for listed files
+  bar = ProgressBar.new(linecount, :bar, :counter, :elapsed, :rate)
+
   f.each_line do |line|
     locations = line.split(" ")
     sync_target_sh(locations[0], locations[1])
+    bar.increment!
   end
   f.close
 end
@@ -17,6 +30,11 @@ def print_file(file_path)
       puts line
     end
   end
+end
+
+def sync_target_sh(file, sync_location)
+  cmd = "rsync -az " + file + " " + sync_location
+  value = %x[ #{cmd} ]
 end
 
 def sync_target(file, sync_location)
@@ -31,12 +49,6 @@ def sync_target(file, sync_location)
   end
 end
 
-def sync_target_sh(file, sync_location)
-  cmd = "rsync -az " + file + " " + sync_location
-  value = %x[ #{cmd} ]
-  puts value
-end
-
 def append_target(file_path, file, location)
   f = File.open(file_path, 'a')
   f << file_path + " " + location
@@ -44,7 +56,7 @@ end
 
 def main()
   file = "targets"
-  sync_target_sh(file)
+  sync_list(file)
 end
 
 main()
